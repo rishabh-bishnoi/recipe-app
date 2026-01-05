1e9fab327f47eee0c618c262727e30bf04bea5cb
rishabh-bishnoi <rishabh.bishnoi@icloud.com>
2026-01-05 10:36:08 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '0a54b0db-4887-4e24-9d38-1c6487772e10';
+export const MODAL_CLOSE_SEC = 2.5;
